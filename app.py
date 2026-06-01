207eeed9415948ec54a4fe968d9d55c6d735b80e
komaga-ux <komaga@gmail.com>
2026-06-01 16:03:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -379,5 +379,7 @@ def inject_global_model_vars():
         model_info=model_info
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # يقرأ البورت من السيرفر، وإذا لم يجده (محلياً) يشتغل على 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
